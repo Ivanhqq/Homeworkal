9566023900440a59ee2f2ebca4cff7d10a472800
Ivan <ivanazanov@gmail.com>
2023-04-03 23:17:21 +0500

    ДЗ

---
+++ work1.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+public class work1 {
+
+    public static void main(String[] args) {
+
+        // Генерация случайных чисел
+        LinkedList<Integer> ll = new LinkedList<>();
+        
+        for (int i = 0; i < 10; i++) {
+            Random r = new Random(); 
+            ll.add(i, r.nextInt(100));
+        }
+
+        System.out.println(ll);
+        
+        LinkedList<Integer> nll = new LinkedList<>();
+        for (int i = ll.size()-1; i >=0; i--) {
+            nll.add(ll.get(i));
+        }
+        System.out.println(nll);
+
+    }
+}
